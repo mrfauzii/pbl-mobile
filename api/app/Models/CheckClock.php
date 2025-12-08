f92d3e5db0c39df09379289e74eab85a32dd2b46
Audric Andhika <audricc015@gmail.com>
2025-12-08 12:55:45 +0700

    refactor(models): perbaikan struktur model

--- api/app/Models/CheckClock.php
+++ api/app/Models/CheckClock.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CheckClock extends Model
 {
+    protected $table = 'check_clocks';
+    
     protected $fillable = [
         'employee_id', 'check_clock_type', 'date', 'clock_in', 'clock_out',
         'overtime_start', 'overtime_end', 'latitude', 'longitude', 'accuracy_meters'
@@ -17,6 +19,11 @@ class CheckClock extends Model
         'clock_in' => 'datetime:H:i',
         'clock_out' => 'datetime:H:i',
         'check_clock_type' => 'boolean',
+        'overtime_start' => 'datetime:H:i',
+        'overtime_end' => 'datetime:H:i',
+        'latitude' => 'float',
+        'longitude' => 'float',
+        'accuracy_meters' => 'integer',
     ];
 
     public function employee(): BelongsTo
--- api/app/Models/Holiday.php
+++ api/app/Models/Holiday.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Holiday extends Model
 {
@@ -12,4 +13,28 @@ class Holiday extends Model
     protected $casts = [
         'date' => 'date',
     ];
+
+    public function translatedDayOfWeek(): Attribute
+    {
+        return Attribute::get(function () {
+            if (!$this->date) {
+                return null;
+            }
+
+            $english = $this->date->format('l');
+            $days = [
+                'Monday'    => 'Senin',
+                'Tuesday'   => 'Selasa',
+                'Wednesday' => 'Rabu',
+                'Thursday'  => 'Kamis',
+                'Friday'    => 'Jumat',
+                'Saturday'  => 'Sabtu',
+                'Sunday'    => 'Minggu',
+            ];
+
+            return $days[$english] ?? $english;
+        });
+    }
+
+    protected $appends = ['translated_day_of_week'];
 }
