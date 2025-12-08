c81591ddb6dbd32b37bcb8d59f764fbf8d8cc649
Audric Andhika <audricc015@gmail.com>
2025-12-08 13:00:08 +0700

    feat(api): menambahkan rute untuk absen dan jadwal

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\AttendanceController;
+use App\Http\Controllers\API\ScheduleController;
 use App\Http\Controllers\API\DepartementController;
 use App\Http\Controllers\API\PasswordChangeController;
 use App\Http\Controllers\API\PositionController;
@@ -25,3 +27,14 @@ Route::post("/register", [AuthController::class, "register"])->middleware(
 Route::post("/send-token", [PasswordChangeController::class, "send_token"]);
 Route::post("/check-token", [PasswordChangeController::class, "check_token"]);
 Route::post("/change-password", [PasswordChangeController::class, "change_password"]);
+
+Route::get('/absen/status', [AttendanceController::class, 'statusHariIni']);
+Route::post('/absen/in', [AttendanceController::class, 'clockIn']);
+Route::post('/absen/out', [AttendanceController::class, 'clockOut']);
+Route::post('/lembur/in', [AttendanceController::class, 'lemburIn']);
+Route::post('/lembur/out', [AttendanceController::class, 'lemburOut']);
+
+Route::prefix('schedule')->group(function () {
+    Route::get('/year/{year?}', [ScheduleController::class, 'getYearSchedule']);
+    Route::post('/holiday', [ScheduleController::class, 'addHoliday']);
+});
